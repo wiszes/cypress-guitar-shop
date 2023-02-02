05a2522b667bb1a8f9b3879cf73bdb1046e6e1d6
wiszes <marcinmiszta25@gmail.com>
2023-02-02 18:04:14 +0100

    Commit

--- cypress/support/page-object/globalPage.js
+++ cypress/support/page-object/globalPage.js
@@ -7,15 +7,8 @@ class GlobalPage {
     this.acceptConsentBtn.trigger("mouseover").click();
   }
   visitPage() {
-    // cy.clearAllSessionStorage();
-    // cy.intercept("POST", "https://www.thomann.de/pl/cookie_storage.html").as(
-    //   "aac"
-    // );
-    // cy.intercept("POST", "https://www.thomann.de/pl/cla.html").as("aac1");
     cy.visit("/index.html");
     this.clickOnAcceptConsentBtn();
-    // cy.wait("@aac");
-    // cy.wait("@aac1");
   }
 }
 
